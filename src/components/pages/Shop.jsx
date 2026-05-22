913f7963394e990deb28ce4fe36f7bc035d60eb3
Ayesha Sangani <142738168+AyeshaSangani@users.noreply.github.com>
2026-05-22 16:43:49 +0500

    Refactor Shop component and clean up code

--- src/components/pages/Shop.jsx
+++ src/components/pages/Shop.jsx
@@ -1,60 +1,87 @@
-import React, { useEffect , useState} from 'react'
+import React, { } from "react";
+import { useEffect, useState } from "react";
+import "./Shop.css"
 
 function Shop() {
 
+  const [product, setProduct] = useState([])
 
-      const [product, setProduct] = useState([])
+  useEffect(() => {
 
-        useEffect(() => {
+    fetch('https://fakestoreapi.com/products/')
+      .then(res => res.json())
+      .then(data => { setProduct(data) })
 
-          fetch('https://fakestoreapi.com/products')  
-            .then(res => res.json())
-            .then(data => {setProduct(data)})
-        }, [])
-      
-      console.log(product);
+  }, [])
 
-
+  console.log(product);
 
   return (
-
     <>
-<div class="container">
+      <div id="carouselExample" className="carousel slide">
+        <div className="carousel-inner">
+          <div className="carousel-item active">
+            <img src="./images/b1" className="d-block w-100" alt="..." />
+          </div>
 
-    <div class="content">
-      
-        <h1> <u> Feature Product </u></h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque, perferendis est. Mollitia ipsum ut officia?</p>
-        
-    </div>
-
+        </div>
 
 
-<div class="row container-fluid" id="all-cards">
+      </div>
+
+      <div className="container p-5">
+
+        <div className="product-heading p-5 text-center">
+          <h1>
+            <u> Our Feature Product </u>
+          </h1>
+
+          <hr />
+        </div>
+
+
+        <div className="show-products row justify-content-center">
+
+
     
-    <div class="card mx-2 my-2" style="width: 15rem;">
-        <div class=" box box1">
-            <img src="${element.image}" class=" my-3 card-img-top" alt="..."/>
+        {
+          product.map((item)=>(
+
+              <div className="col-md-3 mb-3" key={item.id} >
+            <div className="card" style={{ width: "14rem" }}>
+              <div className="box box1">
+                <img src={item.image} className="card-img-top" alt="..." />
+              </div>
+
+              <div className="card-body">
+                <h5 className="card-title">{item.title}</h5>
+
+                <p className="card-text">{item.category}</p>
+
+                <p className="card-text"><b><span>Rs.</span>{item.price}</b></p>
+
+                <a href="#" className="btn btn-warning"> Add To Cart </a>
+              </div>
+            </div>
+          </div>
+          
+          ))
+        }
+
+          
+
+         
+
         </div>
-        <div class="card-body">
-          <h5 class="card-title">${element.category}</h5>
-          <p class="card-text">${element.description}</p>
-          <p>Rs. <del>1,500</del> <strong>${element.price}</strong></p>
-          <a href="#" class="btn btn-outline-info">Go somewhere</a>
-        </div>
-    </div> 
 
-</div>
+        
 
 
-
-
-</div>
-
-
+      </div>
     </>
 
-  )
+
+  );
 }
 
-export default Shop
+export default Shop;
